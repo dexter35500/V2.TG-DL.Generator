f71f840940bb294f6173845ab58bbe5b7daf333a
dexter35500 <143913153+dexter35500@users.noreply.github.com>
2026-05-25 18:36:35 -0300

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -1,13 +1,13 @@
 import logging
-from pyrogram import filters
+from pyrogram import filters, Client
 from pyrogram.types import Message
 from vars import Var
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 @filters.on_message(filters.command("start") & filters.private)
-async def start_command(client, message: Message):
-    # Usamos "client" directamente (que es tu StreamBot corriendo)
+async def start_command(client: Client, message: Message):
+    # Usamos la instancia inyectada nativamente en la variable "client"
     logging.info(f"Comando /start recibido de {message.from_user.id}")
     
     await message.reply_text(
